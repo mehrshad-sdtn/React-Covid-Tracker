af05e4bbaed87a9713314ae93d64b9fc1196d08b
Mehrshad Saadatinia <63510622+mehrshad-sdtn@users.noreply.github.com>
2021-12-24 21:31:53 +0330

    initial commit

---
+++ covid_tracker/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import CovidData from './CovidData';
+//Mehrshad Saadatinia - 97243039
+
+function App() {
+  return ( 
+    <div className="App">
+      <CovidData/>
+    </div>
+  );
+}
+
+export default App;
